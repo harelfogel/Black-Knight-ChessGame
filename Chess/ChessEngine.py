f1ebc3a1d073e729fd854245f36786d87ca8718c
YovelElad <yuvel.elad@gmail.com>
2021-12-06 14:34:22 +0200

    third video

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -23,6 +23,58 @@ class GameState():
         self.whiteToMove = not self.whiteToMove #swap players (switch turns)
 
 
+    '''
+    Undo the last move
+    '''
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptures
+            self.whiteToMove = not self.whiteToMove  # swap players (switch turns)
+
+
+    '''
+    All moves considering checks
+    '''
+    def getValidMoves(self):
+        return self.getAllPosibleMoves() #for now we will not worry about checks.
+
+    '''
+    All moves without considering checks
+    '''
+    def getAllPosibleMoves(self):
+        moves = [Move((6,4), (4,4), self.board)]
+        for r in range(len(self.board)): #number of rows
+            for c in range(len(self.board[r])): #number of col in given row
+                turn = self.board[r][c][0] #return the color of a piece at a specific square
+                if(turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1] #return the type of the piece
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    if piece == 'R':
+                        self.getRookMoves(r, c, moves)
+        return moves
+
+
+    '''
+    Get all the pawn moves for the pawn located at row, col and add these moves to the list
+    '''
+    def getPawnMoves(selfself, r, c, moves):
+        pass
+
+    '''
+    Get all the rook moves for the pawn located at row, col and add these moves to the list
+    '''
+
+    def getRookMoves(selfself, r, c, moves):
+        pass
+
+
+
+
+
+
 class Move():
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
                    "5": 3, "6": 2, "7": 1, "8": 0}
@@ -37,6 +89,16 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptures = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+
+    '''
+    Overriding the equals method
+    '''
+    def __eq__(self, other):
+        if isinstance(other,Move):
+            return self.moveID == other.moveID
+        return  False
 
 
     def getChessNotation(self):
--- Chess/main.py
+++ Chess/main.py
@@ -22,6 +22,8 @@ def main():
     clock = p.time.Clock
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False #flag variable for when a move is made
     loadImages()
     running = True
     sqSelected = () #no square selected, keep track of the last click of the user (tuple: (rew,col))
@@ -30,6 +32,7 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            #mouse handler
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos() #(x,y) location of the mouse
                 col = location[0]//SQ_SIZE
@@ -43,9 +46,22 @@ def main():
                 if len(playerClicks) == 2: #after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = () #reset user clicks
                     playerClicks = []
+
+            #key handler
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: #undo when 'z' is pressed
+                    print("undo")
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
 
         # clock.tick(MAX_FPS)
         p.display.flip()
